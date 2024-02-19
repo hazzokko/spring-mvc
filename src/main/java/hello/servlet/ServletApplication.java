59480f48b298a2a5fa366b699823c5c865197ee5
hazzokko <hazzokko@gmail.com>
2024-02-19 19:46:47 +0900

    5. Spring MVC 시작

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan // 스프링이 자동으로 현재 패키지와 하위 패키지를 순회하며 서블릿을 등록
 @SpringBootApplication
@@ -11,4 +13,10 @@ public class ServletApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }
+
+//    3.0 버전부터는 RequestMapping으로 컨트롤러를 인식하지 않으므로 주석처리
+//    @Bean
+//    SpringMemberFormControllerV1 springMemberFormControllerV1() {
+//        return new SpringMemberFormControllerV1();
+//    }
 }
---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // response를 사용하지는 않지만 파라미터로 받을 수 있음을 보여주기 위해서 넣음
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+}
